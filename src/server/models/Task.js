196a550a634785a05a6ead63395b09ae5fc938f1
Belous Nikita <belousnikita@gmail.com>
2019-04-22 14:48:34 +0300

    task schema and addition

--- src/server/models/Task.js
+++ src/server/models/Task.js
@@ -1,8 +1,9 @@
-import { Schema } from "mongoose";
+import { Schema, model } from "mongoose";
 
 const TaskSchema = new Schema({
   message: { type: String, required: true },
   createdAt: { type: Date, required: true }
 });
+const Task = model("Task", TaskSchema);
 
 export default TaskSchema;
--- src/server/utils/DatabaseUtils.js
+++ src/server/utils/DatabaseUtils.js
@@ -1,8 +1,10 @@
 import { model, connect } from "mongoose";
 import config from '../../etc/config.json';
 import "../models/Column";
+import '../models/Task';
 
 const Column = model("Column");
+const Task = model("Task");
 
 export function setUpConnection() {
   connect(`mongodb://${config.db.host}:${config.db.port}/${config.db.name}`, { useNewUrlParser: true });
@@ -11,14 +13,27 @@ export function setUpConnection() {
 export function listColumns() {
   return Column.find();
 }
-
+export function getColumn(id) {
+  return Column.findById(id);
+}
 export function createColumn(data) {
-    const column = new Column({
-        title: data.title, 
-        tasks: []
-    })
-    return column.save();
+  const column = new Column({
+    title: data.title,
+    tasks: []
+  })
+  return column.save();
+}
+export function addTask(columnId, taskData) {
+  return Column.findByIdAndUpdate(columnId, {
+    $push: {
+      tasks: new Task({
+        message: taskData.message,
+        createdAt: taskData.date
+      }
+      )
+    }
+  });
 }
 export function deleteColumn(id) {
-    return Column.findById(id).deleteOne();
+  return Column.findById(id).deleteOne();
 }
