4921efd8c13111ee80ae31e40e1bf6389a0f5c55
Belous Nikita <belousnikita@gmail.com>
2019-04-22 14:47:25 +0300

    Card date and time added

--- src/client/components/Card/Card.jsx
+++ src/client/components/Card/Card.jsx
@@ -4,21 +4,34 @@ import './card.scss';
 export default class Card extends React.Component {
   constructor(props) {
     super(props);
+    this.date = new Date(props.createdAt);
+    this.day = this.date.getDate().toString(10);
+    this.month = this.date.getMonth().toString(10);
+    this.year = this.date.getFullYear().toString(10);
+    this.hours = this.date.getHours().toString(10);
+    this.minutes = this.date.getMinutes().toString(10);
   }
   render() {
+    const { day, month, year, hours, minutes } = this;
+    console.log({ day, month, year, hours, minutes });
     return (
       <div className="card">
-      <button
+        <button
           className="card_deleteButton"
           onClick={() => console.log("card delete")}
         >
           <i className="far fa-times-circle"></i>
         </button>
         <p className="card_text">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          {this.props.message}
         </p>
-        <p className="card_date">19.12.2019</p>
+        <div className="card_text">
+        <div className="card_date">
+          <p className="card_date_time">{`${hours}:${minutes}`}</p>
+          <p className="card_date_day"> {`${day.lenght < 2 ? `0${day}` : day}.${month.length < 2 ? `0${month}` : month}.${year}`}</p>
+        </div>
       </div>
+      </div>    
     );
   }
 }
