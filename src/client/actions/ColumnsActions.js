78e31f8a8968d026917cbee34d5e45ed9a98a415
Belous Nikita <belousnikita@gmail.com>
2019-04-23 10:49:39 +0300

    prettier

--- src/client/actions/ColumnsActions.js
+++ src/client/actions/ColumnsActions.js
@@ -1,68 +1,55 @@
-import AppDispatcher from '../dispatcher/AppDispatcher';
-import Constants from '../constants/AppConstants';
+import AppDispatcher from "../dispatcher/AppDispatcher";
+import Constants from "../constants/AppConstants";
 
-import api from '../api';
-
+import api from "../api";
 
 const ColumnsActions = {
-    loadColumns() {
+  loadColumns() {
+    AppDispatcher.dispatch({
+      type: Constants.LOAD_COLUMNS_REQUEST
+    });
+
+    api
+      .listColumns()
+      .then(({ data }) =>
         AppDispatcher.dispatch({
-            type: Constants.LOAD_COLUMNS_REQUEST
-        });
+          type: Constants.LOAD_COLUMNS_SUCCESS,
+          columns: data
+        })
+      )
+      .catch(err =>
+        AppDispatcher.dispatch({
+          type: Constants.LOAD_COLUMNS_FAIL,
+          error: err
+        })
+      );
+  },
 
-        api.listColumns()
-            .then(({ data }) =>
-                AppDispatcher.dispatch({
-                    type: Constants.LOAD_COLUMNS_SUCCESS,
-                    columns: data
-                })
-            )
-            .catch(err =>
-                AppDispatcher.dispatch({
-                    type: Constants.LOAD_COLUMNS_FAIL,
-                    error: err
-                })
-            );
-    },
+  createColumn(column) {
+    api
+      .createColumn(column)
+      .then(() => this.loadColumns())
+      .catch(err => console.error(err));
+  },
 
-    createColumn(column) {
-        api.createColumn(column)
-            .then(() =>
-                this.loadColumns()
-            )
-            .catch(err =>
-                console.error(err)
-            );
-    },
-
-    deleteColumn(columnId) {
-        api.deleteColumn(columnId)
-            .then(() =>
-                this.loadColumns()
-            )
-            .catch(err =>
-                console.error(err)
-            );
-    },
-    addTask(columnId, task) {
-        api.addTaskToColumn(columnId, task)
-            .then(
-                () => this.loadColumns()
-            )
-            .catch(
-                err => console.error(err)
-            );
-    },
-    deleteTask(columnId, taskId) {
-
-        api.deleteTask(columnId, taskId)
-            .then(
-                () => this.loadColumns()
-            )
-            .catch(
-                err => console.error(err)
-            );
-    },
+  deleteColumn(columnId) {
+    api
+      .deleteColumn(columnId)
+      .then(() => this.loadColumns())
+      .catch(err => console.error(err));
+  },
+  addTask(columnId, task) {
+    api
+      .addTaskToColumn(columnId, task)
+      .then(() => this.loadColumns())
+      .catch(err => console.error(err));
+  },
+  deleteTask(columnId, taskId) {
+    api
+      .deleteTask(columnId, taskId)
+      .then(() => this.loadColumns())
+      .catch(err => console.error(err));
+  }
 };
 
 export default ColumnsActions;
