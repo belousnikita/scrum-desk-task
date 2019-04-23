94b7f303968bd333eb5f2bc2c486fca5db5d5510
Belous Nikita <belousnikita@gmail.com>
2019-04-23 10:49:25 +0300

    drag and drop wrapper

--- src/client/main.js
+++ src/client/main.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from './components/App/App.jsx';
+import App from "./components/App/App.jsx";
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+import { DragDropContextProvider } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
+
+ReactDOM.render(
+  <DragDropContextProvider backend={HTML5Backend}>
+    <App />
+  </DragDropContextProvider>,
+  document.getElementById("root")
+);
