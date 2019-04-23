8c5085938f554e418099427273bb2c1af17a5b46
Belous Nikita <belousnikita@gmail.com>
2019-04-23 10:50:46 +0300

    drag and drop added

--- src/client/components/Card/Card.jsx
+++ src/client/components/Card/Card.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-import './card.scss';
 
-export default class Card extends React.Component {
+import ItemTypes from "../../../etc/itemTypes.json";
+import { DragSource } from "react-dnd";
+
+import "./card.scss";
+
+class Card extends React.Component {
   constructor(props) {
     super(props);
     this.date = new Date(props.createdAt);
@@ -13,27 +17,44 @@ export default class Card extends React.Component {
   }
   render() {
     const { day, month, year, hours, minutes } = this;
-    console.log({ day, month, year, hours, minutes });
-    return (
-      <div className="card">
-        <button
-          className="card_deleteButton"
-          onClick={this.props.onDelete}
-        >
-          <i className="far fa-times-circle"></i>
+    return this.props.connectDragSource(
+      <div className={`card ${this.props.isDragging ? "drag" : null}`}>
+        <button className="card_deleteButton" onClick={this.props.onDelete}>
+          <i className="far fa-times-circle" />
         </button>
-        <p className="card_text">
-          {this.props.message}
-        </p>
+        <p className="card_text">{this.props.message}</p>
         <div className="card_text">
           <div className="card_date">
             <p className="card_date_time">
-            {`${hours.length < 2 ? `0${hours}` : hours}:${minutes.length < 2 ? `0${minutes}` : minutes}`}</p>
-            <p className="card_date_day"> 
-            {`${day.lenght < 2 ? `0${day}` : day}.${month.length < 2 ? `0${month}` : month}.${year}`}</p>
+              {`${hours.length < 2 ? `0${hours}` : hours}:${
+                minutes.length < 2 ? `0${minutes}` : minutes
+              }`}
+            </p>
+            <p className="card_date_day">
+              {`${day.lenght < 2 ? `0${day}` : day}.${
+                month.length < 2 ? `0${month}` : month
+              }.${year}`}
+            </p>
           </div>
         </div>
       </div>
     );
   }
 }
+export default DragSource(
+  ItemTypes.CARD,
+  {
+    beginDrag: props => ({ id: props.id, parentId: props.parentId }),
+    endDrag(props, monitor) {
+      const item = monitor.getItem();
+      const dropResult = monitor.getDropResult();
+      if (dropResult) {
+        props.onMove(props.parentId, dropResult.id);
+      }
+    }
+  },
+  (connect, monitor) => ({
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+  })
+)(Card);
