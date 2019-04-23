e01bf00a17c6763c4a1b3ddf97949d7b40c930e9
Belous Nikita <belousnikita@gmail.com>
2019-04-23 10:50:15 +0300

    prettier

--- src/client/api/index.js
+++ src/client/api/index.js
@@ -1,25 +1,33 @@
-import axios from 'axios';
+import axios from "axios";
 
-import { apiPrefix } from '../../etc/config.json';
+import { apiPrefix } from "../../etc/config.json";
 
 export default {
-    listColumns() {
-        return axios.get(`${apiPrefix}/columns`);
-    },
+  listColumns() {
+    return axios.get(`${apiPrefix}/columns`);
+  },
 
-    createColumn(data) {
-        return axios.post(`${apiPrefix}/columns`, { type: "column", ...data });
-    },
+  createColumn(data) {
+    return axios.post(`${apiPrefix}/columns`, { type: "column", ...data });
+  },
 
-    deleteColumn(columnId) {
-        return axios.delete(`${apiPrefix}/columns/${columnId}/${false}`);
-    },
+  deleteColumn(columnId) {
+    return axios.delete(`${apiPrefix}/columns/${columnId}/${false}`);
+  },
 
-    addTaskToColumn(columnId, data) {
-        return axios.post(`${apiPrefix}/columns`, { type: "task", id: columnId, ...data });
-    },
-    
-    deleteTask(columnId, taskId) {
-        return axios.delete(`${apiPrefix}/columns/${columnId}/${taskId}`, { type: "task",  columnId, taskId });
-    }
-}
+  addTaskToColumn(columnId, data) {
+    return axios.post(`${apiPrefix}/columns`, {
+      type: "task",
+      id: columnId,
+      ...data
+    });
+  },
+
+  deleteTask(columnId, taskId) {
+    return axios.delete(`${apiPrefix}/columns/${columnId}/${taskId}`, {
+      type: "task",
+      columnId,
+      taskId
+    });
+  }
+};
