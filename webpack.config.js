d2aff4e0d3b962662749720f1d8cf9b008b704d2
Belous Nikita <belousnikita@gmail.com>
2019-04-23 11:24:45 +0300

    ports now loading from config

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,6 @@
 var webpack = require("webpack");
 var path = require('path');
-// module.exports = {
-//   entry: "./src/client/main.js",
-//   output: {
-//     path: __dirname + "/public/build/",
-//     publicPath: "build/",
-//     filename: "bundle.js"
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         loader: "babel-loader",
-//         exclude: [/node_modules/, /public/],
-//         options: {
-//           presets: ['@babel/preset-react']
-//         }
-//       },
-//       {
-//         test: /\.css$/,
-//         loader: "sass-loader!css-loader!autoprefixer-loader",
-//         exclude: [/node_modules/, /public/]
-//       },
-//       {
-//         test: /\.less$/,
-//         loader: "style-loader!css-loader!autoprefixer-loader!sass",
-//         exclude: [/node_modules/, /public/]
-//       },
-//       {
-//         test: /\.gif$/,
-//         loader: "url-loader?limit=10000&mimetype=image/gif"
-//       },
-//       {
-//         test: /\.jpg$/,
-//         loader: "url-loader?limit=10000&mimetype=image/jpg"
-//       },
-//       {
-//         test: /\.png$/,
-//         loader: "url-loader?limit=10000&mimetype=image/png"
-//       },
-//       {
-//         test: /\.svg/,
-//         loader: "url-loader?limit=26000&mimetype=image/svg+xml"
-//       },
-//       {
-//         test: /\.jsx$/,
-//         loader: "react-hot!babel",
-//         exclude: [/node_modules/, /public/]
-//       },
-//       {
-//         test: /\.json$/,
-//         loader: "json-loader"
-//       }
-//     ]
-//   }
-// };
+var config = require('./src/etc/config.json');
 module.exports = {
   mode: "development",
   entry: "./src/client/main.js",
@@ -66,7 +12,7 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'src/public/'),
     compress: true,
-    port: 9000
+    port: config.pagePort,
   },
   watch: true,
   module: {
