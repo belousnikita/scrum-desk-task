3f9332d0b21b2df79d0954762b94b015c22ee439
Belous Nikita <belousnikita@gmail.com>
2019-04-23 10:52:41 +0300

    prettier

--- src/server/app.js
+++ src/server/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import * as dataBase from "../server/utils/DatabaseUtils";
-import cors from 'cors';
+import cors from "cors";
 import { serverPort } from "../etc/config.json";
 
 dataBase.setUpConnection();
@@ -11,13 +11,11 @@ const app = express();
 app.use(bodyParser.json());
 
 // Allow requests from any origin
-app.use(cors({ origin: '*' }));
-
+app.use(cors({ origin: "*" }));
 
 app.get("/columns", (request, response) => {
   dataBase.listColumns().then(data => response.send(data));
 });
-
 
 app.post("/columns", (request, response) => {
   switch (request.body.type) {
@@ -26,17 +24,21 @@ app.post("/columns", (request, response) => {
       return;
     }
     case "task": {
-      dataBase.addTask(request.body.id, request.body).then(data => response.send(data));
+      dataBase
+        .addTask(request.body.id, request.body)
+        .then(data => response.send(data));
       return;
     }
-    default: return;
+    default:
+      return;
   }
-
 });
 
 app.delete("/columns/:id/:task", (request, response) => {
   if (request.params.task !== "false") {
-    dataBase.deleteTask(request.params.id, request.params.task).then(data => response.send(data));
+    dataBase
+      .deleteTask(request.params.id, request.params.task)
+      .then(data => response.send(data));
   } else {
     dataBase.deleteColumn(request.params.id).then(data => response.send(data));
   }
