47cf63a5e239753fb5c8a229bda68d8793850e62
Belous Nikita <belousnikita@gmail.com>
2019-04-23 10:51:14 +0300

    drag and drop target added

--- src/client/components/Column/Column.jsx
+++ src/client/components/Column/Column.jsx
@@ -1,19 +1,19 @@
 import React from "react";
+import { DropTarget } from "react-dnd";
+import ItemTypes from "../../../etc/itemTypes.json";
 import "./column.scss";
 
-export default class Column extends React.Component {
+class Column extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return (
-      <div className="column">
+    const isActive = this.props.canDrop && this.props.isOver;
+    return this.props.connectDropTarget(
+      <div className={`column ${isActive ? "active" : null}`}>
         <p className="column_title">{this.props.title}</p>
-        <button
-          className="column_deleteButton"
-          onClick={this.props.onDelete}
-        > 
-          <i className="fas fa-trash"></i>
+        <button className="column_deleteButton" onClick={this.props.onDelete}>
+          <i className="fas fa-trash" />
         </button>
         {this.props.children}
         <button
@@ -26,3 +26,14 @@ export default class Column extends React.Component {
     );
   }
 }
+export default DropTarget(
+  ItemTypes.CARD,
+  {
+    drop: props => ({ id: props.id })
+  },
+  (connect, monitor) => ({
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver(),
+    canDrop: monitor.canDrop()
+  })
+)(Column);
